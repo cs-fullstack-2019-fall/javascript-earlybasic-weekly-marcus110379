db34779274729014e6d8513e85156c27871a2db8
marcus110379 <marcusjmadden@gmail.com>
2019-08-09 15:55:23 -0500

    code

---
+++ index.js
@@ -0,0 +1,31 @@
+// Ask the user to enter to enter an option, then go to that option (2 points):
+// Example:
+// "Welcome to Kenn's program. Press 1 to count. Press 2 to use a calculator. Press q to quit".
+
+// #### When selecting count by pressing 1 (2 points):
+// Ask the user to count by ones.
+// - If they select count by ones, ask them to enter a number. Start at 1 and count up to that number in the console log.
+
+// #### When selecting use a calculator by pressing 2 (2 points):
+// Ask the user if they want to use addition.
+// - If they select 'a', ask them to enter two numbers. Give them the sum of the numbers.
+
+// var isvalid = false;
+// while(isvalid){
+// var userInput = prompt("Welcome to Marcus's program. Press 1 to count. Press 2 to use a calculator. Press q to quit.");
+// 	if(userInput === 1 || userInput === 2 || userInput = q){
+//           isvalid = true;
+// }
+//    }
+
+var isValid = false;
+
+while(!isValid){
+	
+    var userInput = prompt("Welcome to Marcus's program. Press 1 to count. press 2 to use a calculator. press q to quit.");
+	if(parseInt(userInput) === 1 || parseInt(userInput) === 2 || userInput === "q"){
+	 	isValid = true;
+	 }if(userInput === 1){
+	 	var select1s = prompt(" do you want to count by ones?")
+	 }
+}
